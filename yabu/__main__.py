b3e9ed396b28084b7d24e63f617fb953ef1a9ea7
Roberto Bochet <robertobochet@gmail.com>
2020-10-29 22:03:31 +0100

    Improved logging setup

--- yabu/__main__.py
+++ yabu/__main__.py
@@ -4,40 +4,37 @@ import logging
 import sys
 
 from yabu import YABU
-from yabu.exceptions import InvalidConfig, ConfigError
+from yabu.exceptions import ConfigError
 from .log import logger_setup
 
+_LOGGER = logging.getLogger(__package__)
+
 if __name__ == "__main__":
-    # Gets inline arguments
+    # gets inline arguments
     parser = argparse.ArgumentParser(prog="yabu")
 
     parser.add_argument("-c", "--config", dest="config_path", default="/etc/yabu/config.yaml",
                         help="configuration file path")
     parser.add_argument("-v", dest="log_level", action="count", default=0,
                         help="number of -v defines level of verbosity")
-    parser.add_argument("--info", dest="log_level", action="store_const", const=2, help="equal to -vv")
-    parser.add_argument("--debug", dest="log_level", action="store_const", const=3, help="equal to -vvv")
 
-    # Parses args
+    # parses args
     args = vars(parser.parse_args())
 
-    # Parses the verbosity level
-    logger_setup({
-                     0: logging.ERROR,
-                     1: logging.WARNING,
-                     2: logging.INFO,
-                     3: logging.DEBUG
-                 }[args["log_level"]])
+    # sets verbosity level
+    logger_setup(args["log_level"])
 
-    # Removes no longer necessary log level from args
+    # removes no longer necessary log level from args
     del args["log_level"]
 
-    # Creates an instance of YABU
+    # creates an instance of YABU
     try:
         yabu = YABU(**args)
     except ConfigError as e:
-        print(e)
+        _LOGGER.critical("Configuration issue: I give up")
         sys.exit(1)
 
-    # Starts backup operations
+    _LOGGER.info("YABU is ready to start")
+
+    # starts backup operations
     yabu.start()

1644f3749b246bfbad61299e46100bb262e6a806
Roberto Bochet <robertobochet@gmail.com>
2020-10-29 02:36:33 +0100

    Added schema.yaml to setup.py

--- setup.py
+++ setup.py
@@ -3,27 +3,30 @@ from setuptools import setup, find_packages
 __version__ = "0.0"
 exec(open("./yabu/version.py").read())
 
-with open('README.md') as f:
+with open("README.md") as f:
     _LONG_DESCRIPTION = f.read()
 
 setup(
-    name='yabu',
+    name="yabu",
     packages=find_packages(),
     version=__version__,
-    license='gpl-3.0',
-    description='yet another backup utility',
+    license="gpl-3.0",
+    description="yet another backup utility",
     long_description=_LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    author='Roberto Bochet',
-    author_email='robertobochet@gmail.com',
-    url='https://github.com/RobertoBochet/yabu',
-    keywords=['backup', 'rsync'],
+    long_description_content_type="text/markdown",
+    author="Roberto Bochet",
+    author_email="robertobochet@gmail.com",
+    url="https://github.com/RobertoBochet/yabu",
+    keywords=["backup", "rsync"],
     install_requires=[
         "yamale ~= 3.0.4"
     ],
+    package_data={
+        "yabu.yabu": ["config.schema.yaml"]
+    },
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3'
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3"
     ],
-    python_requires='>=3.8'
+    python_requires=">=3.8"
 )
